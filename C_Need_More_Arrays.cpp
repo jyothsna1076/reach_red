708e9d9c338440847a33dca7676c80fc5ece67b8
jyothsna <jyothsnavadlamudi2008@gmail.com>
2025-06-29 09:22:26 +0530

    yesterday, the problem i struggled with so much is actually easy

--- C_Need_More_Arrays.cpp
+++ C_Need_More_Arrays.cpp
@@ -4,9 +4,31 @@ int main()
 {
     int tc;
     cin>>tc;
-    while(tc--)
+    int m= 1;
+    while(m<=tc)
     {
-        
+      int n;
+      cin>>n;
+      vector<int> arr(n);
+      for(auto &a: arr) cin>>a;
+    //   if(m==158)
+    //   {
+    //     for( auto a:arr) cout<<a<<" ";
+    //     cout<<endl;
+    //   }
+      int ans=1;
+     // bool entered= false;
+     int prev=0;
+      for(int i=1;i<n;i++)
+      {
+        if(arr[i]> arr[prev]+1) 
+        {
+          ans++;
+          prev= i;
+        }
+      }
+      cout<<ans<<endl;
+      m++;
     }
     return 0;
 }
